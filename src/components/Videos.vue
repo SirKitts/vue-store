8bf2ea174943e5abca355842b823fb55e511e85a
SirKitts <sirkitts@gmail.com>
2017-06-22 00:25:48 +1000

    added input range sample

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="videos">
     {{msg}} <input v-model="selectedVideo.store.video.id">
+    <br/>
+    <input 
+      type="range" 
+      min="12345" 
+      max="98765" 
+      step="1" 
+      v-model="videoSelector"
+      @change="updateMe" />
     <pre>{{$data | json}}</pre>
   </div>
 </template>
@@ -13,7 +21,14 @@ export default {
   data () {
     return {
       msg: 'Videos',
-      selectedVideo: store
+      selectedVideo: store,
+      videoSelector: '12345'
+    }
+  },
+  methods: {
+    updateMe () {
+      // this.$emit('updateMe', foo)
+      this.selectedVideo.store.video.id = this.videoSelector
     }
   }
 }
